d09563cdf9a351b28d480ed97ff1248214926eb2
Sowmya <sowmyabs.ec@gmail.com>
2023-01-23 20:22:23 +0530

    updating react

--- React_Jobportal/src/Components/Profile.js
+++ React_Jobportal/src/Components/Profile.js
@@ -27,24 +27,42 @@ function Profile() {
         <div className="prof-content">
           <div className="prof-input">
             <h2>Please update your profile: </h2><br></br>
+            <div className="prof-row">
             <label>Email : </label>
             <input type="text" name='email' onChange={handleChange} /><br></br>
+            </div>   
+            <div className="prof-row"> 
             <label>Name : </label>
             <input type="text" name='name' onChange={handleChange} /><br></br>
+            </div>
+            <div className="prof-row">
             <label>Phone number: </label>
             <input type="text" name='phno' onChange={handleChange} /><br></br>
+            </div>
+            <div className="prof-row">
             <label>Current Company : </label>
             <input type="text" name='currentcomp' onChange={handleChange} /><br></br>
+            </div>
+            <div className="prof-row">
             <label>Current Role : </label>
             <input type="text" name='currentrole' onChange={handleChange} /><br></br>
+            </div>
+            <div className="prof-row">
             <label>About: </label>
             <textarea name="about" rows='2' cols='40' onChange={handleChange} ></textarea><br></br>
+            </div>
+            <div className="prof-row">
             <label>Upload your profile picture: </label>
             <input type="file" name="profpic" onChange={handleChange} /><br></br>
+            </div>
+            <div className="prof-row">
             <label>Upload your CV :</label>
             <input type="file" name="profcv" onChange={handleChange} /><br></br>
+            </div>
+            <div className="prof-row">
             <label>Skills: </label>
             <textarea name="skills" rows='2' cols='40' onChange={handleChange} ></textarea><br></br>
+            </div>
             <input type="submit" onClick={sendData}></input>
           </div>
 
